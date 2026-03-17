87ecc170d8c2845cb5c16ce71f23aa9d1b2ff1e6
Riya Sarkar <riyasarkar017@proton.me>
2026-03-17 12:03:41 +0530

    Create Program.cs

---
+++ src/DotnetBilling.API/Program.cs
@@ -0,0 +1,37 @@
+using System.Text.Json.Serialization;
+using DotnetBilling.Application.Interfaces;
+using DotnetBilling.Infrastructure;
+using DotnetBilling.API.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
+    await seeder.SeedAsync();
+}
+
+app.Run();
