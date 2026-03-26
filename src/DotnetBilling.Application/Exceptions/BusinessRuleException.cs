64147f86316fe6c80c52b8edaac01c8fa604ddae
Riya Sarkar <riyasarkar017@proton.me>
2026-03-26 09:07:36 +0530

    Create BusinessRuleException.cs

---
+++ src/DotnetBilling.Application/Exceptions/BusinessRuleException.cs
@@ -0,0 +1,8 @@
+namespace DotnetBilling.Application.Exceptions;
+
+public class BusinessRuleException : Exception
+{
+    public BusinessRuleException(string message) : base(message)
+    {
+    }
+}
